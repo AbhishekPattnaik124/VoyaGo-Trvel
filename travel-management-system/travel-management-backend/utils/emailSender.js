fff9060de4ee586f2b9b2fce0135c08d2f378212
AbhishekPattnaik124 <abhishekpattnaik612@gmail.com>
2026-04-10 22:25:36 +0530

    fix: update nodemailer to use Gmail service instead of SMTP

--- travel-management-system/travel-management-backend/utils/emailSender.js
+++ travel-management-system/travel-management-backend/utils/emailSender.js
@@ -5,13 +5,11 @@ dotenv.config();
 
 // Create a transporter object using SMTP transport
 const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_PORT == 465, // Use 'true' for port 465 (SSL)
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 });
 
 /**
